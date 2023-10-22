b83ba2b77618ab80270882eb5b29abe607fa505d
Juan David Arboleda <jarboleda@vortexbird.com>
2023-10-22 18:00:42 -0500

    Initial commit

---
+++ src/test/java/com/trafilea/test/coffeeshop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafilea.test.coffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
